5bc3d40396461d75224f73e1d690d1ce5ba80c04
waxy88 <101388015+waxy88@users.noreply.github.com>
2022-03-13 17:19:33 +0000

    Add tests

---
+++ CalastoneCandidateTest.Test/LessThanThreeFilterTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using CalastoneCandidateTest.Extensions;
+using CalastoneCandidateTest.Filters;
+
+
+namespace CalastoneCandidateTest.Test
+{
+    public class LessThanThreeFilterTests
+    {
+        [Test]
+        public void Returns_true_if_word_is_less_than_three()
+        {
+            var lttf = new LessThanThreeFilter();
+
+            Assert.IsTrue(lttf.Filter("so").IsNullOrWhiteSpace());
+        }
+    }
+}
